a8cdfbaa22b89806d294e97493b8f5c6ef5a28f9
Adnan RIHAN <adnan@rihan.fr>
2025-01-09 16:27:05 +0100

    Fix tests for Laravel >= 10

--- tests/DatabaseSQLiteSchemaGrammarTest.php
+++ tests/DatabaseSQLiteSchemaGrammarTest.php
@@ -2,17 +2,36 @@
 
 namespace Mx\Sqfix\Tests;
 
-use Illuminate\Database\Connection;
+use Composer\InstalledVersions;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Mockery as m;
+use Mx\Sqfix\Connection as SqfixConnection;
 use Mx\Sqfix\SchemaGrammar;
 
 class DatabaseSQLiteSchemaGrammarTest extends TestCase
 {
+    private static $laravelVersion;
+
+    public static function setUpBeforeClass(): void
+    {
+        $version = explode('.', InstalledVersions::getVersion('laravel/framework'));
+
+        foreach ($version as &$v) {
+            $v = intval($v);
+        }
+
+        self::$laravelVersion = $version;
+    }
+
     protected function tearDown(): void
     {
         m::close();
+    }
+
+    public function testServiceProviderLoaded()
+    {
+        $this->assertInstanceOf(SqfixConnection::class, $this->getConnection());
     }
 
     public function testBasicCreateTable()
@@ -48,7 +67,12 @@ class DatabaseSQLiteSchemaGrammarTest extends TestCase
         $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
 
         $this->assertCount(1, $statements);
-        $this->assertSame('create table "users" ("id" integer not null primary key autoincrement, "email" varchar not null)', $statements[0]);
+
+        $expectedModifiers = 'not null primary key autoincrement';
+        if (static::$laravelVersion[0] >= 10) {
+            $expectedModifiers = 'primary key not null autoincrement';
+        }
+        $this->assertSame('create table "users" ("id" integer '.$expectedModifiers.', "email" varchar not null)', $statements[0]);
 
         $blueprint = new Blueprint('users');
         $blueprint->increments('id')->trueSyntax();
@@ -57,7 +81,7 @@ class DatabaseSQLiteSchemaGrammarTest extends TestCase
 
         $this->assertCount(2, $statements);
         $expected = [
-            'alter table "users" add column "id" integer not null primary key autoincrement',
+            'alter table "users" add column "id" integer '.$expectedModifiers,
             'alter table "users" add column "email" varchar not null',
         ];
         $this->assertEquals($expected, $statements);
@@ -86,7 +110,12 @@ class DatabaseSQLiteSchemaGrammarTest extends TestCase
         $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
 
         $this->assertCount(1, $statements);
-        $this->assertSame('create temporary table "users" ("id" integer not null primary key autoincrement, "email" varchar not null)', $statements[0]);
+
+        $expectedModifiers = 'not null primary key autoincrement';
+        if (static::$laravelVersion[0] >= 10) {
+            $expectedModifiers = 'primary key not null autoincrement';
+        }
+        $this->assertSame('create temporary table "users" ("id" integer '.$expectedModifiers.', "email" varchar not null)', $statements[0]);
     }
 
     public function testAddingPrimaryKey()
